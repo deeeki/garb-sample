908657de27b5cbaa23809fdde5a72e4eeb1406e8
deeeki <deeeki+gh@gmail.com>
2011-07-03 17:50:31 +0900

    New: add metrics of site usage

--- get_results.rb
+++ get_results.rb
@@ -12,6 +12,12 @@ access_token = OAuth::AccessToken.new(consumer, ACCESS_TOKEN, ACCESS_SECRET)
 Garb::Session.access_token = access_token
 profile = Garb::Management::Profile.all.detect {|p| p.web_property_id == PROFILE}
 
+class SiteUsage
+	extend Garb::Model
+
+	metrics :visitors, :percent_new_visits, :visits, :avg_time_on_site, :pageviews, :pageviews_per_visit, :visit_bounce_rate
+end
+
 class PageTitle
 	extend Garb::Model
 
@@ -39,11 +45,23 @@ defaults = {
 	:start_date => target_date,
 	:end_date => target_date,
 }
+rs_su = profile.site_usage(defaults)
 rs_pt = profile.page_title(defaults.merge(:sort => :pageviews.desc))
 rs_k = profile.keyword(defaults.merge(:sort => :visits.desc))
 rs_rp = profile.referral_path(defaults.merge(:sort => :visits.desc))
 
 print "# Results for " + target_date.to_s + "\n\n"
+print "# SiteUsage\n####################\n\n"
+rs_su.each do |r|
+	print r.visits + " visits\n"
+	print r.pageviews + " pageviews\n"
+	print r.visitors + " visitors\n"
+	print r.pageviews_per_visit + " pages/visit\n"
+	print r.visit_bounce_rate + " bounce rate\n"
+	print r.avg_time_on_site + " avg. time on site\n"
+	print r.percent_new_visits + " % new visits\n"
+	print "\n"
+end
 print "# PageTitle\n####################\n\n"
 rs_pt.each do |r|
 	print r.pageviews + "\n" + r.page_title + "\n" + r.hostname + r.page_path + "\n\n"
