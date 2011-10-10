12101139810cca9910ecac1657d51083db5fc0a3
deeeki <deeeki+gh@gmail.com>
2011-10-10 21:10:55 +0900

    support target profile by CLI argument

--- get_results.rb
+++ get_results.rb
@@ -1,76 +1,91 @@
 require File.expand_path('../boot', __FILE__)
 
 consumer = OAuth::Consumer.new CONSUMER_KEY, CONSUMER_SECRET, {
-	:signature_method   => 'HMAC-SHA1',
-	:site               => 'https://www.google.com',
-	:request_token_path => '/accounts/OAuthGetRequestToken',
-	:authorize_path     => '/accounts/OAuthAuthorizeToken',
-	:access_token_path  => '/accounts/OAuthGetAccessToken',
+  :signature_method   => 'HMAC-SHA1',
+  :site               => 'https://www.google.com',
+  :request_token_path => '/accounts/OAuthGetRequestToken',
+  :authorize_path     => '/accounts/OAuthAuthorizeToken',
+  :access_token_path  => '/accounts/OAuthGetAccessToken',
 }
 access_token = OAuth::AccessToken.new(consumer, ACCESS_TOKEN, ACCESS_SECRET)
 
+if ARGV[0]
+  id = ARGV[0]
+else
+  id = PROFILE
+end
+
 Garb::Session.access_token = access_token
-profile = Garb::Management::Profile.all.detect {|p| p.web_property_id == PROFILE}
+profile = Garb::Management::Profile.all.detect {|p| p.web_property_id == id}
+puts 'set arg Analytics profile id you wanto to check' and exit if profile.nil?
 
 class SiteUsage
-	extend Garb::Model
+  extend Garb::Model
 
-	metrics :visitors, :percent_new_visits, :visits, :avg_time_on_site, :pageviews, :pageviews_per_visit, :visit_bounce_rate
+  metrics :visitors, :percent_new_visits, :visits, :avg_time_on_site, :pageviews, :pageviews_per_visit, :visit_bounce_rate
 end
 
 class PageTitle
-	extend Garb::Model
+  extend Garb::Model
 
-	metrics :pageviews
-	dimensions :hostname, :page_path, :page_title
+  metrics :pageviews
+  dimensions :hostname, :page_path, :page_title
 end
 
 class Keyword
-	extend Garb::Model
+  extend Garb::Model
 
-	metrics :visits
-	dimensions :keyword
+  metrics :visits
+  dimensions :keyword
 end
 
 class ReferralPath
-	extend Garb::Model
+  extend Garb::Model
 
-	metrics :visits
-	dimensions :referral_path, :source
+  metrics :visits
+  dimensions :referral_path, :source
 end
 
 target_date = Date.today - 1
 defaults = {
-	:limit => 20,
-	:start_date => target_date,
-	:end_date => target_date,
+  :limit => 20,
+  :start_date => target_date,
+  :end_date => target_date,
 }
 rs_su = profile.site_usage(defaults)
 rs_pt = profile.page_title(defaults.merge(:sort => :pageviews.desc))
 rs_k = profile.keyword(defaults.merge(:sort => :visits.desc))
 rs_rp = profile.referral_path(defaults.merge(:sort => :visits.desc))
 
-print "# Results for " + target_date.to_s + "\n\n"
-print "# SiteUsage\n####################\n\n"
+puts "# Results for " + target_date.to_s
+puts
+puts "# SiteUsage\n####################"
+puts
 rs_su.each do |r|
-	print r.visits + " visits\n"
-	print r.pageviews + " pageviews\n"
-	print r.visitors + " visitors\n"
-	print r.pageviews_per_visit + " pages/visit\n"
-	print r.visit_bounce_rate + " bounce rate\n"
-	print r.avg_time_on_site + " avg. time on site\n"
-	print r.percent_new_visits + " % new visits\n"
-	print "\n"
+  puts r.visits + " visits"
+  puts r.pageviews + " pageviews"
+  puts r.visitors + " visitors"
+  puts r.pageviews_per_visit + " pages/visit"
+  puts r.visit_bounce_rate + " bounce rate"
+  puts r.avg_time_on_site + " avg. time on site"
+  puts r.percent_new_visits + " % new visits"
+  puts
 end
-print "# PageTitle\n####################\n\n"
+puts "# PageTitle\n####################"
+puts
 rs_pt.each do |r|
-	print r.pageviews + "\n" + r.page_title + "\n" + r.hostname + r.page_path + "\n\n"
+  puts r.pageviews + "\n" + r.page_title + "\n" + r.hostname + r.page_path
+  puts
 end
-print "# Keyword\n####################\n\n"
+puts "# Keyword\n####################"
+puts
 rs_k.each do |r|
-	print r.visits + "\n" + r.keyword + "\n\n"
+  puts r.visits + "\n" + r.keyword
+  puts
 end
-print "# ReferralPath\n####################\n\n"
+puts "# ReferralPath\n####################"
+puts
 rs_rp.each do |r|
-	print r.visits + "\n" + r.source + r.referral_path + "\n\n"
+  puts r.visits + "\n" + r.source + r.referral_path
+  puts
 end
