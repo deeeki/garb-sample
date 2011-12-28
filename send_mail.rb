529ba717dce055871baac605f3819ba5f0cac156
deeeki <deeeki+gh@gmail.com>
2011-12-28 23:53:14 +0900

    fix sendmail options

--- send_mail.rb
+++ send_mail.rb
@@ -2,7 +2,10 @@
 require File.expand_path('../boot', __FILE__)
 
 Pony.mail(
-  :location => '/usr/sbin/sendmail',
+  :via => :sendmail,
+  :via_options => {
+    :location => '/usr/sbin/sendmail',
+  },
   :from => MAIL_FROM,
   :to => MAIL_TO,
   :subject => "Analytics Summary #{Date.today - 1}",
