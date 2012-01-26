9d67a325d5da7a5647222c7867748caba06789ba
deeeki <deeeki+gh@gmail.com>
2012-01-26 23:50:41 +0900

    cleanup legacy code

--- boot.rb
+++ boot.rb
@@ -3,6 +3,5 @@ $:.unshift File.dirname(__FILE__)
 require 'bundler/setup'
 Bundler.require(:default) if defined?(Bundler)
 
-require 'config'
 require 'lib/classes'
 require 'lib/reporter'
--- send_mail.rb
+++ send_mail.rb
@@ -1,13 +1,14 @@
 # coding: utf-8
 require File.expand_path('../boot', __FILE__)
+@config_mail ||= YAML.load_file 'config/mail.yml'
 
 Pony.mail(
   :via => :sendmail,
   :via_options => {
     :location => '/usr/sbin/sendmail',
   },
-  :from => MAIL_FROM,
-  :to => MAIL_TO,
+  :from => @config_mail['from'],
+  :to => @config_mail['to'],
   :subject => "Analytics Summary #{Date.today - 1}",
   :body => STDIN.read,
   :charset => 'utf-8'
